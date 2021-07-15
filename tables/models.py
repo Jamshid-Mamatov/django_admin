6120b382afac0f1742b398aa00ee23d4a823d1e1
Jamshid <mamatovjamshid506@gmail.com>
2021-07-15 10:44:25 +0500

    create superuser

--- tables/models.py
+++ tables/models.py
@@ -1,3 +1,18 @@
 from django.db import models
-
+from django.db.models.deletion import CASCADE
+from django.utils import timezone
+from django.contrib.auth.models import User
 # Create your models here.
+class Language(models.Model):
+    name=models.CharField(max_length=20)
+    date_created=models.DateField(default=timezone.now)
+    def __str__(self):
+        return self.name
+
+class Framework(models.Model):
+    name=models.CharField(max_length=20)
+    language=models.ForeignKey(Language,on_delete=models.CASCADE)
+    date_created=models.DateField(default=timezone.now)
+    
+    def __str__(self):
+        return self.name
